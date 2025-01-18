f63c5d1aea66daaa42f4833e54516ead999b9fe9
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-01-18 13:43:47 +0200

    Initial Commit

---
+++ SwiftfulThinkingSPMsAndSDKs/SwiftfulThinkingSPMsAndSDKsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingSPMsAndSDKsApp.swift
+//  SwiftfulThinkingSPMsAndSDKs
+//
+//  Created by Toni Stoyanov on 18.01.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingSPMsAndSDKsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
