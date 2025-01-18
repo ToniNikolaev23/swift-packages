ea341b43e87102d848f5ed7f1585d251caf34502
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-01-18 14:24:22 +0200

    SDWebImage

--- SwiftfulThinkingSPMsAndSDKs/SDWebImageBootcamp.swift
+++ SwiftfulThinkingSPMsAndSDKs/SDWebImageBootcamp.swift
@@ -6,10 +6,67 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
+
+// https://picsum.photos/id/237/200/300
+
+struct ImageLoader: View {
+    let url: String
+    var contentMode: ContentMode = .fill
+    
+    var body: some View {
+        Rectangle()
+            .opacity(0)
+            .overlay(
+                SDWebImageLoader(url: url, contentMode: contentMode)
+                    .allowsHitTesting(false)
+            )
+            .clipped()
+
+    }
+}
+
+fileprivate struct SDWebImageLoader: View {
+    
+    let url: String
+    var contentMode: ContentMode = .fill
+    
+    var body: some View {
+        WebImage(url: URL(string: url))
+            .onSuccess(perform: { image, data, cacheType in
+                
+            })
+            .onFailure(perform: { error in
+                
+            })
+            .resizable()
+            .aspectRatio(contentMode: contentMode)
+            
+    }
+}
+
+final class ImagePrefetcher {
+    static let instance = ImagePrefetcher()
+    
+    private let prefetcher = SDWebImagePrefetcher()
+    
+    private init() {}
+    
+    func startPrefetching(urls: [URL]) {
+        prefetcher.prefetchURLs(urls)
+    }
+    
+    func stopPrefetching() {
+        prefetcher.cancelPrefetching()
+    }
+}
 
 struct SDWebImageBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ImageLoader(url: "https://picsum.photos/id/237/200/300", contentMode: .fill)
+            .frame(width: 200, height: 200)
+        
+        
     }
 }
 
